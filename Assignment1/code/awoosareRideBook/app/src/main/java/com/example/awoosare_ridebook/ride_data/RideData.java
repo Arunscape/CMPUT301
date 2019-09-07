9c062c0f0e53b61276e12dcc96abca0520f30e2e
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-06 22:27:09 -0600

    rename dummy data

--- Assignment1/code/awoosareRideBook/app/src/main/java/com/example/awoosare_ridebook/ride_data/RideData.java
+++ Assignment1/code/awoosareRideBook/app/src/main/java/com/example/awoosare_ridebook/ride_data/RideData.java
@@ -1,4 +1,4 @@
-package com.example.awoosare_ridebook.dummy;
+package com.example.awoosare_ridebook.ride_data;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,7 +11,7 @@ import java.util.Map;
  * <p>
  * TODO: Replace all uses of this class before publishing your app.
  */
-public class DummyContent {
+public class RideData {
 
     /**
      * An array of sample (dummy) items.
