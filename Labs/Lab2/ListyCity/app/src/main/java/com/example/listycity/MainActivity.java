a8644f8b2d83e05bbf0130dddb6377d33141e55a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-15 19:34:54 -0600

    hey it works

--- Labs/Lab2/ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ Labs/Lab2/ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.listycity;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cityList = findViewById(R.id.city_list);
+
+        String[] cities = {"Edmonton", "Calgary", "Ottawa", "Winnipeg", "Victoria", "Montreal", "Toronto", "St. John's", "Charlottetown", "Halifax"};
+
+        dataList = new ArrayList<>();
+
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, R.id.content_view, dataList);
+
+        cityList.setAdapter(cityAdapter);
+
     }
 }
