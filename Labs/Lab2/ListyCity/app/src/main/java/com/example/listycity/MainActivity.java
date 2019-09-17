b946c850e04708418f9e1ee190a3b7c2ba601e41
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-16 18:05:03 -0600

    nice

--- Labs/Lab2/ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ Labs/Lab2/ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    LinearLayout textEdit ;
+    EditText textBox;
+
+    String selectedCity = "Edmonton";
 
 
     @Override
@@ -33,4 +41,45 @@ public class MainActivity extends AppCompatActivity {
         cityList.setAdapter(cityAdapter);
 
     }
+
+    public void showTextEdit(View v){
+        textEdit = findViewById(R.id.textEditView);
+        textEdit.setVisibility(View.VISIBLE);
+
+        textBox = findViewById((R.id.textbox));
+        textBox.requestFocus();
+
+        showKeyboard();
+    }
+
+    private void showKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+        imm.showSoftInput(textBox, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    protected void hideTextEdit(){
+        textEdit =  findViewById(R.id.textEditView);
+        textEdit.setVisibility(View.GONE);
+    }
+
+    public void confirmAdd(View v){
+        textBox = findViewById(R.id.textbox);
+
+        String city = textBox.getText().toString();
+        dataList.add(city);
+
+
+        hideKeyboard();
+
+        hideTextEdit();
+
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+        View view = this.getCurrentFocus();
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+
 }
