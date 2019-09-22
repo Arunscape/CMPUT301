0b82df505adfe21122e9f5e80b8716640cdf6aea
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-22 15:42:44 -0600

    ridedetail now shows up in the detail panel

--- Assignments/Assignment1/awoosare-RideBook/app/src/main/java/com/example/awoosare_ridebook/ItemDetailFragment.java
+++ Assignments/Assignment1/awoosare-RideBook/app/src/main/java/com/example/awoosare_ridebook/ItemDetailFragment.java
@@ -60,11 +60,13 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.item_detail, container, false);
+//        View rootView = inflater.inflate(R.layout.item_detail, container, false)
+        View rootView = inflater.inflate(R.layout.ride_detail, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+//            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+
         }
 
         return rootView;
