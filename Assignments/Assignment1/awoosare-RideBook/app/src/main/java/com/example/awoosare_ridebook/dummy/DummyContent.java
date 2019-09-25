8d1480e90961c6d44fc7f98912085e334a5558d0
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-24 18:11:11 -0600

    cleanup dummycontent

--- Assignments/Assignment1/awoosare-RideBook/app/src/main/java/com/example/awoosare_ridebook/dummy/DummyContent.java
+++ Assignments/Assignment1/awoosare-RideBook/app/src/main/java/com/example/awoosare_ridebook/dummy/DummyContent.java
@@ -1,90 +1,20 @@
 package com.example.awoosare_ridebook.dummy;
-
-import android.icu.util.Calendar;
-
 import com.example.awoosare_ridebook.Ride;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class DummyContent {
 
-    /**
-     * An array of sample (dummy) items.
-     */
     public static final List<Ride> ITEMS = new ArrayList<Ride>();
 
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
     public static final Map<String, Ride> ITEM_MAP = new HashMap<String, Ride>();
 
     public static void addItem(Ride item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.getId(), item);
     }
-
-    //TODO temporary for filling in content
-//    static {
-//        // Add 3 sample items.
-//        addItem(new Ride(new Calendar() {
-//            @Override
-//            protected int handleGetLimit(int i, int i1) {
-//                return 0;
-//            }
-//
-//            @Override
-//            protected int handleComputeMonthStart(int i, int i1, boolean b) {
-//                return 0;
-//            }
-//
-//            @Override
-//            protected int handleGetExtendedYear() {
-//                return 0;
-//            }
-//        }, 1, 2, 3, 4, "") );
-//        addItem(new Ride(new Calendar() {
-//            @Override
-//            protected int handleGetLimit(int i, int i1) {
-//                return 0;
-//            }
-//
-//            @Override
-//            protected int handleComputeMonthStart(int i, int i1, boolean b) {
-//                return 0;
-//            }
-//
-//            @Override
-//            protected int handleGetExtendedYear() {
-//                return 0;
-//            }
-//        }, 5, 6, 7, 8, "ride 2's comment") );
-//        addItem(new Ride(new Calendar() {
-//            @Override
-//            protected int handleGetLimit(int i, int i1) {
-//                return 0;
-//            }
-//
-//            @Override
-//            protected int handleComputeMonthStart(int i, int i1, boolean b) {
-//                return 0;
-//            }
-//
-//            @Override
-//            protected int handleGetExtendedYear() {
-//                return 0;
-//            }
-//        }, 88, 75, 4, 6, "ride 3's comment") );
-//    }
-    //TODO temporary for filling in content
 
 }
